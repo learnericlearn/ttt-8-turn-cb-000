42f71f1550bb62d1c9573ee866da0a36dcf266ef
Eric Frommelt <ericfrommelt@gmail.com>
2018-05-23 00:34:55 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+def turn(board)
+  puts "Please enter 1-9:"
+end
+
+#Helper methods
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  return user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index)
+    return false
+  end
+  if index.between?(0, 8)
+    return true
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+  elsif board[index] == " " || "" || nil
+    return false
+  end
+end
+
+def move(board, position, token = "X")
+  board[position] = token
+end
